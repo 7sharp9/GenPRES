be11b90688917d48f5e1a0887eaa174b11996b4c
Mark Seemann <mark@ploeh.dk>
2026-07-17 15:31:59 +0200

    refactor(utils): Delete unused constants

--- src/Informedica.Utils.Lib/Constants.fs
+++ src/Informedica.Utils.Lib/Constants.fs
@@ -2,13 +2,8 @@ namespace Informedica.Utils.Lib
 
 
 module Constants =
-    let check = "✅"
-    let crossMark = "❌"
-    let rocket = "🚀"
     let stopwatch = "⏱"
-    let hourglass = "⌛"
     let clock = "🕰"
-    let floppyDisk = "💾"
     let error = "❌"
     let info = "ℹ"
     let warning = "⚠"
